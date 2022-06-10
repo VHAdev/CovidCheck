66dc0997c639f2a225b89a2d57e68509a76931d8
Vitor Hugo <57417513+VHAdev@users.noreply.github.com>
2022-06-10 14:39:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 
 include_once './header.php';
 
-
-
 if(isset($_GET['formulario'])):
     include_once './inc/part-sintomas.php';
 else:
     include_once './inc/part-usuario.php';
 endif;
-
-
 
 include_once './footer.php';
 
